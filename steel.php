f562d6944f7668dcc4f7343bdd57777b301eace9
Matt Beall <mbeall@starverte.com>
2013-01-13 16:56:44 -0700

    Update steel.php
    Include `royalslider.php`

--- steel.php
+++ steel.php
@@ -25,4 +25,6 @@ License URI: http://www.gnu.org/licenses/
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+include_once(WP_PLUGIN_URL . '/steel/royalslider.php')
+
 ?>
