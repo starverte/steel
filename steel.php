898ec6ff8b8f71d52d84d084ebd7b45e4bad5a97
Matt Beall <mbeall@starverte.com>
2013-01-13 15:48:05 -0800

    Create steel.php

---
+++ steel.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Steel
+Plugin URI: //Not yet developed
+GitHub URI: https://github.com/starverte/steel.git
+Description: Core plugin of the Sparks Framework. Includes custom widgets, royalslider, and options menu.
+Author: starverte
+Author URI: http://starverte.com/
+License: GPLv3
+License URI: http://www.gnu.org/licenses/
+
+  Copyright 2013  Star Verte LLC  (email : info@starverte.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+?>
