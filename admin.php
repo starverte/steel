ef17b656c5902b4e336e3e27475aae2df051000f
Matt Beall <mbeall@starverte.com>
2013-01-14 22:39:00 -0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,3 +1,9 @@
-<?php
-   echo "Admin Page Test";
-?>
+<div class="wrap">
+<?php screen_icon(); ?>
+<h2>Sparks Options</h2>
+<form method="post" action="options.php">
+<?php settings_fields( 'myoption-group' );
+do_settings_fields( 'myoption-group' ); ?>
+<?php submit_button(); ?>
+</form>
+</div>
