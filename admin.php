cfa16f915729408a10e98226ad0ecd7de0fad356
Matt Beall <mbeall@starverte.com>
2013-01-14 23:21:31 -0700

    Add basic options page

--- admin.php
+++ admin.php
@@ -1,9 +1,15 @@
-<div class="wrap">
-<?php screen_icon(); ?>
-<h2>Sparks Options</h2>
-<form method="post" action="options.php">
-<?php settings_fields( 'myoption-group' );
-do_settings_fields( 'myoption-group' ); ?>
-<?php submit_button(); ?>
-</form>
-</div>
+	<div class="wrap">
+		<h2>Sparks Options</h2>
+		<form method="post" action="options.php">
+			<?php settings_fields('sparks_options'); ?>
+			<?php $options = get_option('steel_options'); ?>
+			<table class="form-table">
+				<tr valign="top"><th scope="row">PayPal Merchant ID</th>
+					<td><input type="text" name="steel_options[merch_id]" value="<?php echo $options['merch_id']; ?>" /></td>
+				</tr>
+			</table>
+			<p class="submit">
+			<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+			</p>
+		</form>
+	</div>
