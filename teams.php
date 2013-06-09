11ded2a14026472cfb85295568691ef9848cd35b
Matt Beall <mbeall@starverte.com>
2013-06-09 15:04:40 -0500

    Update teams.php

--- teams.php
+++ teams.php
@@ -9,7 +9,7 @@
  */
 
 // Register Custom Post Type
-function sparks_teams_init() {
+function steel_teams() {
 	$labels = array(
 		'name'                => _x( 'Profiles', 'Post Type General Name', 'sparks' ),
 		'singular_name'       => _x( 'Profile', 'Post Type Singular Name', 'sparks' ),
@@ -24,14 +24,14 @@ function sparks_teams_init() {
 		'not_found'           => __( 'No profiles found', 'sparks' ),
 		'not_found_in_trash'  => __( 'No profiles found in trash. Did you check recycling?', 'sparks' ),
 	);
-
+	
 	$rewrite = array(
 		'slug'                => '',
 		'with_front'          => true,
 		'pages'               => false,
 		'feeds'               => false,
 	);
-
+	
 	$args = array(
 		'label'               => __( 'sp_team_profile', 'sparks' ),
 		'description'         => __( 'Member(s) of "Teams"', 'sparks' ),
@@ -52,8 +52,8 @@ function sparks_teams_init() {
 		'rewrite'             => $rewrite,
 		'capability_type'     => 'page',
 	);
-
-	register_post_type( 'sp_team_profile', $args );
+	
+	register_post_type( 'steel_profile', $args );
 	
 	$labels2 = array(
 		'name'                       => _x( 'Teams', 'Taxonomy General Name', 'sparks' ),
@@ -71,13 +71,13 @@ function sparks_teams_init() {
 		'add_or_remove_items'        => __( 'Add or remove teams', 'sparks' ),
 		'choose_from_most_used'      => __( 'Choose from the most used teams', 'sparks' ),
 	);
-
+	
 	$rewrite2 = array(
-		'slug'                       => 'teams',
+		'slug'                       => 'team',
 		'with_front'                 => true,
 		'hierarchical'               => true,
 	);
-
+	
 	$args2 = array(
 		'labels'                     => $labels2,
 		'hierarchical'               => false,
@@ -88,64 +88,43 @@ function sparks_teams_init() {
 		'show_tagcloud'              => false,
 		'rewrite'                    => $rewrite2,
 	);
-
-	register_taxonomy( 'sp_team', 'sp_team_profile', $args2 );
+	
+	register_taxonomy( 'steel_team', 'steel_profile', $args2 );
 }
 
 // Hook into the 'init' action
-add_action( 'init', 'sparks_teams_init', 0 );
+add_action( 'init', 'steel_teams', 0 );
 
 // BEGIN - Create custom fields
-add_action( 'add_meta_boxes', 'sp_teams_add_custom_boxes' );
+add_action( 'add_meta_boxes', 'steel_teams_meta_boxes' );
 
-function sp_teams_add_custom_boxes() {
-	add_meta_box('sp_teams_meta', 'Team Member Profile', 'sp_teams_meta', 'sp_team_profile', 'side', 'high');
-}
+function steel_teams_meta_boxes() { add_meta_box('steel_teams_meta', 'Team Member Profile', 'steel_teams_meta', 'steel_profile', 'side', 'high'); }
 
 /* Team Member Profile */
-function sp_teams_meta() {
+function steel_teams_meta() {
 	global $post;
-	$custom = get_post_custom($post->ID);
-	
-?>
-    <p><label>Name</label> 
-	<input type="text" size="10" name="team_member_name" value="<?php if (isset($custom['team_member_name'])) { echo $custom["team_member_name"] [0]; } ?>" /></p>
-    <p><label>Phone</label> 
-	<input type="tel" size="10" name="team_member_phone" value="<?php if (isset($custom['team_member_phone'])) { echo $custom["team_member_phone"] [0]; } ?>" /></p>
-    <p><label>Email</label> 
-	<input type="email" size="10" name="team_member_email" value="<?php if (isset($custom['team_member_email'])) { echo $custom["team_member_email"] [0]; } ?>" /></p>
-    
-	<?php
+	$custom = get_post_custom($post->ID); ?>
+  
+	<p><label>Name</label><input type="text" size="10" name="profile_name" value="<?php if (isset($custom['profile_name'])) { echo $custom["profile_name"] [0]; } ?>" /></p>
+	<p><label>Phone</label><input type="tel" size="10" name="profile_phone" value="<?php if (isset($custom['profile_phone'])) { echo $custom["profile_phone"] [0]; } ?>" /></p>
+	<p><label>Email</label><input type="email" size="10" name="profile_email" value="<?php if (isset($custom['profile_email'])) { echo $custom["profile_email"] [0]; } ?>" /></p><?php
 }
 
 /* Save Details */
-add_action('save_post', 'save_teams_details');
+add_action('save_post', 'save_steel_profile');
 
-
-function save_teams_details(){
-  global $post;
-  
-  if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE && (isset($post_id)) ) {
-	return $post_id;
-  }
-
-  if( defined('DOING_AJAX') && DOING_AJAX && (isset($post_id)) ) { //Prevents the metaboxes from being overwritten while quick editing.
-	return $post_id;
-  }
-
-  if( ereg('/\edit\.php', $_SERVER['REQUEST_URI']) && (isset($post_id)) ) { //Detects if the save action is coming from a quick edit/batch edit.
-	return $post_id;
-  }
-  // save all meta data
-  if (isset($_POST['team_member_name'])) {
-  	update_post_meta($post->ID, "team_member_name", $_POST["team_member_name"]);
-  }
-  if (isset($_POST['team_member_phone'])) {
-  	update_post_meta($post->ID, "team_member_phone", $_POST["team_member_phone"]);
-  }
-  if (isset($_POST['team_member_email'])) {
-  	update_post_meta($post->ID, "team_member_email", $_POST["team_member_email"]);
-  }
+function save_steel_profile(){
+	global $post;
+	
+	if ( defined('DOING_AUTOSAVE' ) && DOING_AUTOSAVE && (isset($post_id)) ) { return $post_id; }
+	
+	if( defined('DOING_AJAX' ) && DOING_AJAX && (isset($post_id)) ) { return $post_id; } //Prevents the metaboxes from being overwritten while quick editing.
+	
+	if( ereg( '/\edit\.php', $_SERVER['REQUEST_URI'] ) && (isset($post_id)) ) { return $post_id; } //Detects if the save action is coming from a quick edit/batch edit.
+	
+	// Save the data
+	if (isset($_POST['profile_name'])) {update_post_meta($post->ID, "profile_name", $_POST["profile_name"]);}
+	if (isset($_POST['profile_phone'])) {update_post_meta($post->ID, "profile_phone", $_POST["profile_phone"]);}
+	if (isset($_POST['profile_email'])) {update_post_meta($post->ID, "profile_email", $_POST["profile_email"]);}
 }
-// END - Custom Fields
 ?>
