e9ec6300891a35bee6acdf782be08ed0391d2c9a
Matt Beall <mbeall@starverte.com>
2013-06-09 14:50:16 -0500

    Update events.php

--- events.php
+++ events.php
@@ -1,31 +1,12 @@
 <?php
 /*
-Plugin Name: Sparks Events
-Plugin URI: //Not yet developed
-Description: Part of the Sparks Framework. A plugin that adds the ability of an events calendar.
-Version: alpha
-Author: Star Verte LLC
-Author URI: http://www.starverte.com
-License: GPLv2 or later
-
-	Copyright 2013  Star Verte LLC  (email : info@starverte.com)
-	
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License, version 2, as 
-	published by the Free Software Foundation.
-	
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-	
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-	
-	You should have received a copy of the GNU General Public License
-	along with Events.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Creates Events and management interface
+ *
+ * @package Sparks
+ * @sub-package Steel
+ *
+ * @since 0.6.0
+ */
 
 function sparks_events_init() {
   $labels = array(
