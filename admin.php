762bb5bf9881e9cbdba0ba6e95fa82221668dc3b
Matt Beall <mbeall@starverte.com>
2013-06-09 15:51:36 -0500

    Update admin.php

--- admin.php
+++ admin.php
@@ -11,7 +11,7 @@
 <div class="wrap">
   <h2>Sparks Options</h2>
   <form method="post" action="options.php">
-		<?php settings_fields('sparks_options'); ?>
+    <?php settings_fields('sparks_options'); ?>
     <?php $options = get_option('steel_options'); ?>
     <table class="form-table">
     	<tr valign="top"><th scope="row">PayPal Merchant ID</th><td><input type="text" name="steel_options[merch_id]" value="<?php echo $options['merch_id']; ?>" /></td></tr>
