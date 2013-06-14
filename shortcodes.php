cb46241c9e87b01612e78b9c3e3d3d73a14690e6
Matt Beall <mbeall@starverte.com>
2013-06-14 00:08:48 -0500

    Create [columns] and [column] shortcodes

--- shortcodes.php
+++ shortcodes.php
@@ -8,4 +8,20 @@
  * @since 0.7.0
  */
  
+/*
+ * Create [columns] and [column] shortcodes
+ */
+add_shortcode( 'columns', 'columns_shortcode' );
+add_shortcode( 'column', 'column_shortcode' );
+function columns_shortcode( $atts, $content = null ) {
+  extract( shortcode_atts( array( 'num' => 2 ), $atts ) );
+	$new = strip_tags($content, '<a><strong><em><blockquote><code><ol><ul><li>');
+	return '<div class="columns columns-'. esc_attr($num) .'">' . do_shortcode($new) . '</div>';
+}
+function column_shortcode( $atts, $content = null ) {
+	extract( shortcode_atts( array( 'title' => null ), $atts ) );
+	$new = strip_tags($content, '<a><strong><em><blockquote><code><ol><ul><li>');
+	if (isset($title) && !empty($title)) { return '<div class="column"><h3>' . esc_attr($title) .'</h3><p>' . $new . '</p></div>'; }
+	else { return '<div class="column"><p>' . $new . '</p></div>'; }
+}
  ?>
