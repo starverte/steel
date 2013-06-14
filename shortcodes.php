6ee16a0c65245dcd63f4965d5ed5017e07ce9073
Matt Beall <mbeall@starverte.com>
2013-06-14 01:00:10 -0500

    Update shortcodes.php

--- shortcodes.php
+++ shortcodes.php
@@ -9,19 +9,36 @@
  */
  
 /*
- * Create [columns] and [column] shortcodes
+ * Create [column] shortcode
  */
-add_shortcode( 'columns', 'columns_shortcode' );
 add_shortcode( 'column', 'column_shortcode' );
-function columns_shortcode( $atts, $content = null ) {
-	extract( shortcode_atts( array( 'num' => 2 ), $atts ) );
-	$new = strip_tags($content, '<a><strong><em><blockquote><code><ol><ul><li>');
-	return '<div class="columns columns-'. esc_attr($num) .'">' . do_shortcode($new) . '</div>';
-}
 function column_shortcode( $atts, $content = null ) {
-	extract( shortcode_atts( array( 'title' => null ), $atts ) );
+	extract( shortcode_atts( array( 'title' => null, 'cols' => '2', 'alpha' => isset($atts['alpha']), 'omega' => isset($atts['omega']) ), $atts ) );
+	switch ($cols) {
+		case '2': $style = 'span6'; break;
+		case '3': $style = 'span4'; break;
+		case '4': $style = 'span3'; break;
+		case '5': if ($alpha or $omega) { $style = 'span3'; } else { $style = 'span2'; } break;
+		case '6': $style = 'span2'; break;
+		case '7': if ($alpha or $omega) { $style = 'span1'; } else { $style = 'span2'; } break;
+		case '8': if ($alpha or $omega) { $style = 'span3'; } else { $style = 'span1'; } break;
+		case '9': if ($alpha) { $style = 'span3'; } elseif ($omega) { $style = 'span2'; } else { $style = 'span1'; } break;
+		case '10': if ($alpha or $omega) { $style = 'span2'; } else { $style = 'span1'; } break;
+		case '11': if ($alpha) { $style = 'span2'; } else { $style = 'span1'; } break;
+		case '12': $style = 'span1'; break;
+	}
 	$new = strip_tags($content, '<a><strong><em><blockquote><code><ol><ul><li>');
-	if (isset($title) && !empty($title)) { return '<div class="column"><h3>' . esc_attr($title) .'</h3><p>' . $new . '</p></div>'; }
-	else { return '<div class="column"><p>' . $new . '</p></div>'; }
+	if ($alpha) {
+		if (!empty($title)) { return '<div class="row-fluid"><div class="' . $style . '"><h3>' . esc_attr($title) .'</h3><p>' . $new . '</p></div>'; }
+		else { return '<div class="row-fluid"><div class="' . $style . '"><p>' . $new . '</p></div>'; }
+	}
+	elseif ($omega) {
+		if (!empty($title)) { return '<div class="' . $style . '"><h3>' . esc_attr($title) .'</h3><p>' . $new . '</p></div></div>'; }
+		else { return '<div class="' . $style . '"><p>' . $new . '</p></div></div>'; }
+	}
+	else {
+		if (!empty($title)) { return '<div class="' . $style . '"><h3>' . esc_attr($title) .'</h3><p>' . $new . '</p></div>'; }
+		else { return '<div class="' . $style . '"><p>' . $new . '</p></div>'; }	
+	}
 }
  ?>
