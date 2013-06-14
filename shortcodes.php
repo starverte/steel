6e6245d7bd2733f853df35391c0858e1ebd8db38
Matt Beall <mbeall@starverte.com>
2013-06-14 01:01:04 -0500

    Update shortcodes.php

--- shortcodes.php
+++ shortcodes.php
@@ -15,14 +15,14 @@ add_shortcode( 'column', 'column_shortcode' );
 function column_shortcode( $atts, $content = null ) {
 	extract( shortcode_atts( array( 'title' => null, 'cols' => '2', 'alpha' => isset($atts['alpha']), 'omega' => isset($atts['omega']) ), $atts ) );
 	switch ($cols) {
-		case '2': $style = 'span6'; break;
-		case '3': $style = 'span4'; break;
-		case '4': $style = 'span3'; break;
-		case '5': if ($alpha or $omega) { $style = 'span3'; } else { $style = 'span2'; } break;
-		case '6': $style = 'span2'; break;
-		case '7': if ($alpha or $omega) { $style = 'span1'; } else { $style = 'span2'; } break;
-		case '8': if ($alpha or $omega) { $style = 'span3'; } else { $style = 'span1'; } break;
-		case '9': if ($alpha) { $style = 'span3'; } elseif ($omega) { $style = 'span2'; } else { $style = 'span1'; } break;
+		case '2' : $style = 'span6'; break;
+		case '3' : $style = 'span4'; break;
+		case '4' : $style = 'span3'; break;
+		case '5' : if ($alpha or $omega) { $style = 'span3'; } else { $style = 'span2'; } break;
+		case '6' : $style = 'span2'; break;
+		case '7' : if ($alpha or $omega) { $style = 'span1'; } else { $style = 'span2'; } break;
+		case '8' : if ($alpha or $omega) { $style = 'span3'; } else { $style = 'span1'; } break;
+		case '9' : if ($alpha) { $style = 'span3'; } elseif ($omega) { $style = 'span2'; } else { $style = 'span1'; } break;
 		case '10': if ($alpha or $omega) { $style = 'span2'; } else { $style = 'span1'; } break;
 		case '11': if ($alpha) { $style = 'span2'; } else { $style = 'span1'; } break;
 		case '12': $style = 'span1'; break;
