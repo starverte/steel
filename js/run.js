be21837abc2563cefb35e6ff2ea076d622c3cd0e
Matt Beall <mbeall@starverte.com>
2013-06-16 17:06:39 -0500

    Update and rename datepicker.js to run.js

---
+++ js/run.js
@@ -0,0 +1,15 @@
+/*
+ * Run shortcode scripts for front-end
+ *
+ * @package Sparks
+ * @sub-package Steel
+ *
+ * @since 0.6.0
+ */
+
+jQuery(function() {
+    jQuery( ".tooltip" ).tooltip();
+    jQuery( ".popover" ).popover();
+    jQuery( ".accordion" ).accordion();
+    jQuery( ".carousel" ).carousel();
+});
