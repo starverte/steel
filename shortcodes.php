7a352f8ac9b38d9cb92c7a487b904e30af070c51
Matt Beall <mbeall@starverte.com>
2013-06-18 01:45:03 -0500

    Create carousel shortcode

--- shortcodes.php
+++ shortcodes.php
@@ -56,19 +56,148 @@ function popover_shortcode( $atts, $content = null ) {
 	$new_text = strip_tags($text, '<a><strong><em><blockquote><code><ol><ul><li>');
 	$rand = rand();
 	switch ($color) {
-		case 'default' : $btn_color = ''; break;
-		case 'blue' : $btn_color = ' btn-primary'; break;
-		case 'green' : $btn_color = ' btn-success'; break;
-		case 'yellow' : $btn_color = ' btn-warning'; break;
-		case 'red' : $btn_color = ' btn-danger'; break;
-		case 'black' : $btn_color = ' btn-inverse'; break;
+		case 'default'	: $btn_color = '';				      break;
+		case 'blue'			: $btn_color = ' btn-primary';	break;
+		case 'green'		: $btn_color = ' btn-success';	break;
+		case 'yellow'		: $btn_color = ' btn-warning';	break;
+		case 'red'			: $btn_color = ' btn-danger';	  break;
+		case 'black'		: $btn_color = ' btn-inverse';	break;
 	}
 	switch ($size) {
-		case 'default' : $btn_size = ''; break;
-		case 'large' : $btn_size = ' btn-large'; break;
-		case 'small' : $btn_size = ' btn-small'; break;
-		case 'mini' : $btn_size = ' btn-mini'; break;
+		case 'default'	: $btn_size = '';			    break;
+		case 'large'		: $btn_size = ' btn-large';	break;
+		case 'small'		: $btn_size = ' btn-small';	break;
+		case 'mini'			: $btn_size = ' btn-mini';	break;
 	}
 	return '<a class="btn steel-popover' . $btn_color . $btn_size . '" href="#" data-toggle="popover" title="' . $title . '" data-content="' . $new_text . '" data-placement="' . $placement . '">' . $new_content . '</a>';
+}
+
+
+/*
+ * Create [carousel] shortcode
+ */
+add_shortcode('carousel', 'carousel_shortcode');
+function carousel_shortcode($attr) {
+	$post = get_post();
+
+	static $instance = 0;
+	$instance++;
+
+	if ( ! empty( $attr['ids'] ) ) {
+		// 'ids' is explicitly ordered, unless you specify otherwise.
+		if ( empty( $attr['orderby'] ) )
+			$attr['orderby'] = 'post__in';
+		$attr['include'] = $attr['ids'];
+	}
+
+	// Allow plugins/themes to override the default carousel template.
+	$output = apply_filters('post_carousel', '', $attr);
+	if ( $output != '' )
+		return $output;
+
+	// We're trusting author input, so let's at least make sure it looks like a valid orderby statement
+	if ( isset( $attr['orderby'] ) ) {
+		$attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+		if ( !$attr['orderby'] )
+			unset( $attr['orderby'] );
+	}
+
+	extract(shortcode_atts(array(
+		'order'      => 'ASC',
+		'orderby'    => 'menu_order ID',
+		'id'         => $post->ID,
+		'itemtag'    => 'div',
+		'icontag'    => 'div',
+		'captiontag' => 'div',
+		'size'       => 'large',
+		'include'    => '',
+		'exclude'    => ''
+	), $attr));
+
+	$id = intval($id);
+	if ( 'RAND' == $order )
+		$orderby = 'none';
+
+	if ( !empty($include) ) {
+		$_attachments = get_posts( array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+
+		$attachments = array();
+		foreach ( $_attachments as $key => $val ) {
+			$attachments[$val->ID] = $_attachments[$key];
+		}
+	} elseif ( !empty($exclude) ) {
+		$attachments = get_children( array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+	} else {
+		$attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+	}
+
+	if ( empty($attachments) )
+		return '';
+
+	if ( is_feed() ) {
+		$output = "\n";
+		foreach ( $attachments as $att_id => $attachment )
+			$output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+		return $output;
+	}
+
+	$itemtag = tag_escape($itemtag);
+	$captiontag = tag_escape($captiontag);
+	$icontag = tag_escape($icontag);
+	$valid_tags = wp_kses_allowed_html( 'post' );
+	if ( ! isset( $valid_tags[ $itemtag ] ) )
+		$itemtag = 'dl';
+	if ( ! isset( $valid_tags[ $captiontag ] ) )
+		$captiontag = 'dd';
+	if ( ! isset( $valid_tags[ $icontag ] ) )
+		$icontag = 'dt';
+
+	$itemwidth = 100;
+	$float = is_rtl() ? 'right' : 'left';
+
+	$selector = "carousel-{$instance}";
+
+	$carousel_style = $carousel_div = '';
+	if ( apply_filters( 'use_default_carousel_style', true ) )
+		$carousel_style = "";
+	$size_class = sanitize_html_class( $size );
+	$carousel_div = "<div id='myCarousel' class='steel-carousel carousel slide carouselid-{$id}'>";
+	$output = apply_filters( 'carousel_style', $carousel_style . "\n\t\t" . $carousel_div );
+	
+	$output .= "<ol class='carousel-indicators'>";
+	
+	$n = 0;
+	foreach ( $attachments as $id => $attachment ) {
+		$output .= "<li data-target='#myCarousel' data-slide-to='" . ($n++) . "'></li>";
+	}
+	
+	$output .= "</ol><!-- .carousel-indicators -->
+		<div class='carousel-inner'>";
+
+	$i = 0;
+	foreach ( $attachments as $id => $attachment ) {
+		$img_src = wp_get_attachment_image_src($id, $size);
+		$img = "<img src='" . $img_src[0] . "' alt='" . $attachment->alt . "' />";
+
+		$output .= "<{$itemtag} class='item";
+		$i++; if ($i == 0 ) { $output .= " active"; }
+		$output .= "'>";
+		$output .= "$img";
+		if ( $captiontag && trim($attachment->post_excerpt) ) {
+			$output .= "
+				<{$captiontag} class='carousel-caption'>
+				" . wptexturize($attachment->post_excerpt) . "
+				</{$captiontag}>";
+		}
+		$output .= "</{$itemtag}><!-- .item -->";
+	}
+
+	$output .= "
+			</div><!-- .carousel-inner -->
+			<a class='carousel-control left' href='#myCarousel' data-slide='prev'>&lsaquo;</a>
+  		<a class='carousel-control right' href='#myCarousel' data-slide='next'>&rsaquo;</a>
+		</div><!-- #myCarousel -->\n";
+
+	return $output;
 }
 ?>
