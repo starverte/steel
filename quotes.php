4131774847111c7bacfef5992f6f984dce38f40f
Matt Beall <mbeall@starverte.com>
2013-12-11 00:33:52 -0700

    Clean up spacing

--- quotes.php
+++ quotes.php
@@ -12,90 +12,90 @@
  */
 add_action( 'widgets_init', 'steel_quotes_widgets' );
 function steel_quotes_widgets() {
-	register_widget( 'Steel_Quotes_Widget' );
+  register_widget( 'Steel_Quotes_Widget' );
 }
 class Steel_Quotes_Widget extends WP_Widget {
-	
-	function Steel_Quotes_Widget() {
-		$widget_ops = array( 'classname' => 'random-quotes-widget', 'description' => __('Displays a random quote', 'random-quotes-widget') );
-		$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'random-quotes-widget' );
-		$this->WP_Widget( 'random-quotes-widget', __('Steel: Random Quote(s)', 'random-quotes-widget'), $widget_ops, $control_ops );
-	}
+  
+  function Steel_Quotes_Widget() {
+    $widget_ops = array( 'classname' => 'random-quotes-widget', 'description' => __('Displays a random quote', 'random-quotes-widget') );
+    $control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'random-quotes-widget' );
+    $this->WP_Widget( 'random-quotes-widget', __('Steel: Random Quote(s)', 'random-quotes-widget'), $widget_ops, $control_ops );
+  }
 
-	function widget($args, $instance) {
-		// Get list
-		$cat = !empty( $instance['cat'] ) ? get_category( $instance['cat'] ) : false;
+  function widget($args, $instance) {
+    // Get list
+    $cat = !empty( $instance['cat'] ) ? get_category( $instance['cat'] ) : false;
 
-		if ( !$cat )
-			return;
+    if ( !$cat )
+      return;
 
-		$instance['title'] = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
+    $instance['title'] = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
 
-		echo $args['before_widget'];
+    echo $args['before_widget'];
 
-		if ( !empty($instance['title']) )
-			echo $args['before_title'] . $instance['title'] . $args['after_title'];
+    if ( !empty($instance['title']) )
+      echo $args['before_title'] . $instance['title'] . $args['after_title'];
 
-		$quotes = new WP_Query(
-			array(
-				'post_type' => 'post',
-				'orderby' => 'rand',
-				'posts_per_page' => 1,
-				'tax_query' => array(
-				  'relation' => 'AND',
-					array(
-						'taxonomy' => 'category',
-						'field' => 'slug',
-						'terms' => $cat->slug
-					),
-				  array(
-						'taxonomy' => 'post_format',
-						'field' => 'slug',
-						'terms' => array( 'post-format-quote' )
-					)
-				)
-			)
-		);
-		
-		while ($quotes->have_posts()) : $quotes->the_post(); ?>
-    	<blockquote><?php the_content(); ?></blockquote>
+    $quotes = new WP_Query(
+      array(
+        'post_type' => 'post',
+        'orderby' => 'rand',
+        'posts_per_page' => 1,
+        'tax_query' => array(
+          'relation' => 'AND',
+          array(
+            'taxonomy' => 'category',
+            'field' => 'slug',
+            'terms' => $cat->slug
+          ),
+          array(
+            'taxonomy' => 'post_format',
+            'field' => 'slug',
+            'terms' => array( 'post-format-quote' )
+          )
+        )
+      )
+    );
+    
+    while ($quotes->have_posts()) : $quotes->the_post(); ?>
+      <blockquote><?php the_content(); ?></blockquote>
     <?php endwhile;
 
-		echo $args['after_widget'];
-	}
+    echo $args['after_widget'];
+  }
 
-	function update( $new_instance, $old_instance ) {
-		$instance['title'] = strip_tags( stripslashes($new_instance['title']) );
-		$instance['list'] = (int) $new_instance['list'];
-		return $instance;
-	}
+  function update( $new_instance, $old_instance ) {
+    $instance['title'] = strip_tags( stripslashes($new_instance['title']) );
+    $instance['list'] = (int) $new_instance['list'];
+    return $instance;
+  }
 
-	function form( $instance ) {
-		$title = !empty($instance['title']) ? $instance['title'] : '';
-		$list  = !empty($instance['list'])  ? $instance['list']  : '';
+  function form( $instance ) {
+    $title = !empty($instance['title']) ? $instance['title'] : '';
+    $list  = !empty($instance['list'])  ? $instance['list']  : '';
 
-		// Get menus
-		$cats = get_categories();
-		?>
-		<p>
-			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:') ?></label>
-			<input type="text" class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $title; ?>" />
-		</p>
-		<p>
-			<label for="<?php echo $this->get_field_id('list'); ?>"><?php _e('Category:'); ?></label>
-			<select id="<?php echo $this->get_field_id('list'); ?>" name="<?php echo $this->get_field_name('list'); ?>">
-      	<option value="">All Categories</option>
-				<?php 
-					foreach ($cats as $cat) {
-						$option  = '<option value="' . $cat->term_id . '" '. selected( $list, $cat->term_id ) .'>';
-						$option .= $cat->cat_name;
-						$option .= '</option>';
-						echo $option;
-					}
+    // Get menus
+    $cats = get_categories();
+    ?>
+    <p>
+      <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:') ?></label>
+      <input type="text" class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $title; ?>" />
+    </p>
+    <p>
+      <label for="<?php echo $this->get_field_id('list'); ?>"><?php _e('Category:'); ?></label>
+      <select id="<?php echo $this->get_field_id('list'); ?>" name="<?php echo $this->get_field_name('list'); ?>">
+        <option value="">All Categories</option>
+        <?php 
+          foreach ($cats as $cat) {
+            $option  = '<option value="' . $cat->term_id . '" '. selected( $list, $cat->term_id ) .'>';
+            $option .= $cat->cat_name;
+            $option .= '</option>';
+            echo $option;
+          }
         ?>
-			</select>
-		</p>
-		<?php
-	}
+      </select>
+    </p>
+    <?php
+  }
 }
 ?>
--- teams.php
+++ teams.php
@@ -12,86 +12,86 @@
  */
 if (is_module_active('teams')) { add_action( 'init', 'steel_teams_init', 0 ); }
 function steel_teams_init() {
-	$labels = array(
-		'name'                => _x( 'Profiles', 'Post Type General Name', 'steel' ),
-		'singular_name'       => _x( 'Profile', 'Post Type Singular Name', 'steel' ),
-		'menu_name'           => __( 'Teams', 'steel' ),
-		'all_items'           => __( 'All Profiles', 'steel' ),
-		'view_item'           => __( 'View Profile', 'steel' ),
-		'add_new_item'        => __( 'Add New Profile', 'steel' ),
-		'add_new'             => __( 'New Profile', 'steel' ),
-		'edit_item'           => __( 'Edit Profile', 'steel' ),
-		'update_item'         => __( 'Update Profile', 'steel' ),
-		'search_items'        => __( 'Search teams', 'steel' ),
-		'not_found'           => __( 'No profiles found', 'steel' ),
-		'not_found_in_trash'  => __( 'No profiles found in trash. Did you check recycling?', 'steel' ),
-	);
-	
-	$rewrite = array(
-		'slug'                => 'profiles',
-		'with_front'          => true,
-		'pages'               => false,
-		'feeds'               => false,
-	);
-	
-	$args = array(
-		'label'               => __( 'steel_profile', 'steel' ),
-		'description'         => __( 'Member(s) of "Teams"', 'steel' ),
-		'labels'              => $labels,
-		'supports'            => array( 'title', 'editor', 'thumbnail', ),
-		'hierarchical'        => false,
-		'public'              => true,
-		'show_ui'             => true,
-		'show_in_menu'        => true,
-		'show_in_nav_menus'   => true,
-		'show_in_admin_bar'   => true,
-		'menu_position'       => 5,
-		'menu_icon'           => '',
-		'can_export'          => true,
-		'has_archive'         => true,
-		'exclude_from_search' => false,
-		'publicly_queryable'  => true,
-		'rewrite'             => $rewrite,
-		'capability_type'     => 'page',
-	);
-	
-	register_post_type( 'steel_profile', $args );
-	
-	$labels2 = array(
-		'name'                       => _x( 'Teams', 'Taxonomy General Name', 'steel' ),
-		'singular_name'              => _x( 'Team', 'Taxonomy Singular Name', 'steel' ),
-		'menu_name'                  => __( 'Teams', 'steel' ),
-		'all_items'                  => __( 'All Teams', 'steel' ),
-		'parent_item'                => __( '', 'steel' ),
-		'parent_item_colon'          => __( '', 'steel' ),
-		'new_item_name'              => __( 'New Team Name', 'steel' ),
-		'add_new_item'               => __( 'Add New Team', 'steel' ),
-		'edit_item'                  => __( 'Edit Team', 'steel' ),
-		'update_item'                => __( 'Update Team', 'steel' ),
-		'separate_items_with_commas' => __( 'Separate teams with commas', 'steel' ),
-		'search_items'               => __( 'Search teams', 'steel' ),
-		'add_or_remove_items'        => __( 'Add or remove teams', 'steel' ),
-		'choose_from_most_used'      => __( 'Choose from the most used teams', 'steel' ),
-	);
-	
-	$rewrite2 = array(
-		'slug'                       => 'teams',
-		'with_front'                 => true,
-		'hierarchical'               => true,
-	);
-	
-	$args2 = array(
-		'labels'                     => $labels2,
-		'hierarchical'               => false,
-		'public'                     => true,
-		'show_ui'                    => true,
-		'show_admin_column'          => true,
-		'show_in_nav_menus'          => true,
-		'show_tagcloud'              => false,
-		'rewrite'                    => $rewrite2,
-	);
-	
-	register_taxonomy( 'steel_team', 'steel_profile', $args2 );
+  $labels = array(
+    'name'                => _x( 'Profiles', 'Post Type General Name', 'steel' ),
+    'singular_name'       => _x( 'Profile', 'Post Type Singular Name', 'steel' ),
+    'menu_name'           => __( 'Teams', 'steel' ),
+    'all_items'           => __( 'All Profiles', 'steel' ),
+    'view_item'           => __( 'View Profile', 'steel' ),
+    'add_new_item'        => __( 'Add New Profile', 'steel' ),
+    'add_new'             => __( 'New Profile', 'steel' ),
+    'edit_item'           => __( 'Edit Profile', 'steel' ),
+    'update_item'         => __( 'Update Profile', 'steel' ),
+    'search_items'        => __( 'Search teams', 'steel' ),
+    'not_found'           => __( 'No profiles found', 'steel' ),
+    'not_found_in_trash'  => __( 'No profiles found in trash. Did you check recycling?', 'steel' ),
+  );
+  
+  $rewrite = array(
+    'slug'                => 'profiles',
+    'with_front'          => true,
+    'pages'               => false,
+    'feeds'               => false,
+  );
+  
+  $args = array(
+    'label'               => __( 'steel_profile', 'steel' ),
+    'description'         => __( 'Member(s) of "Teams"', 'steel' ),
+    'labels'              => $labels,
+    'supports'            => array( 'title', 'editor', 'thumbnail', ),
+    'hierarchical'        => false,
+    'public'              => true,
+    'show_ui'             => true,
+    'show_in_menu'        => true,
+    'show_in_nav_menus'   => true,
+    'show_in_admin_bar'   => true,
+    'menu_position'       => 5,
+    'menu_icon'           => '',
+    'can_export'          => true,
+    'has_archive'         => true,
+    'exclude_from_search' => false,
+    'publicly_queryable'  => true,
+    'rewrite'             => $rewrite,
+    'capability_type'     => 'page',
+  );
+  
+  register_post_type( 'steel_profile', $args );
+  
+  $labels2 = array(
+    'name'                       => _x( 'Teams', 'Taxonomy General Name', 'steel' ),
+    'singular_name'              => _x( 'Team', 'Taxonomy Singular Name', 'steel' ),
+    'menu_name'                  => __( 'Teams', 'steel' ),
+    'all_items'                  => __( 'All Teams', 'steel' ),
+    'parent_item'                => __( '', 'steel' ),
+    'parent_item_colon'          => __( '', 'steel' ),
+    'new_item_name'              => __( 'New Team Name', 'steel' ),
+    'add_new_item'               => __( 'Add New Team', 'steel' ),
+    'edit_item'                  => __( 'Edit Team', 'steel' ),
+    'update_item'                => __( 'Update Team', 'steel' ),
+    'separate_items_with_commas' => __( 'Separate teams with commas', 'steel' ),
+    'search_items'               => __( 'Search teams', 'steel' ),
+    'add_or_remove_items'        => __( 'Add or remove teams', 'steel' ),
+    'choose_from_most_used'      => __( 'Choose from the most used teams', 'steel' ),
+  );
+  
+  $rewrite2 = array(
+    'slug'                       => 'teams',
+    'with_front'                 => true,
+    'hierarchical'               => true,
+  );
+  
+  $args2 = array(
+    'labels'                     => $labels2,
+    'hierarchical'               => false,
+    'public'                     => true,
+    'show_ui'                    => true,
+    'show_admin_column'          => true,
+    'show_in_nav_menus'          => true,
+    'show_tagcloud'              => false,
+    'rewrite'                    => $rewrite2,
+  );
+  
+  register_taxonomy( 'steel_team', 'steel_profile', $args2 );
 }
 
 /*
@@ -100,12 +100,12 @@ function steel_teams_init() {
 add_action( 'add_meta_boxes', 'steel_teams_meta_boxes' );
 function steel_teams_meta_boxes() { add_meta_box('steel_teams_meta', 'Team Member Profile', 'steel_teams_meta', 'steel_profile', 'side', 'high'); }
 function steel_teams_meta() {
-	global $post;
-	$custom = get_post_custom($post->ID); ?>
+  global $post;
+  $custom = get_post_custom($post->ID); ?>
   
-	<p><label>Title</label><input type="text"  size="10" name="profile_title" value="<?php if (isset($custom['profile_title'])) { echo                                                              $custom["profile_title"][0]; } ?>" /></p>
-	<p><label>Email</label><input type="email" size="10" name="profile_email" value="<?php if (isset($custom['profile_email'])) { echo                                                              $custom["profile_email"][0]; } ?>" /></p>
-	<p><label>Phone</label><input type="tel"   size="10" name="profile_phone" value="<?php if (isset($custom['profile_phone'])) { echo preg_replace("/([0-9]{3})([0-9]{3})([0-9]{4})/", "$1.$2.$3", $custom["profile_phone"][0]); } ?>" /></p><?php
+  <p><label>Title</label><input type="text"  size="10" name="profile_title" value="<?php if (isset($custom['profile_title'])) { echo                                                              $custom["profile_title"][0]; } ?>" /></p>
+  <p><label>Email</label><input type="email" size="10" name="profile_email" value="<?php if (isset($custom['profile_email'])) { echo                                                              $custom["profile_email"][0]; } ?>" /></p>
+  <p><label>Phone</label><input type="tel"   size="10" name="profile_phone" value="<?php if (isset($custom['profile_phone'])) { echo preg_replace("/([0-9]{3})([0-9]{3})([0-9]{4})/", "$1.$2.$3", $custom["profile_phone"][0]); } ?>" /></p><?php
 }
 
 /*
@@ -113,34 +113,34 @@ function steel_teams_meta() {
  */
 add_action('save_post', 'save_steel_profile');
 function save_steel_profile() {
-	global $post;
-	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE && (isset($post_id))) { return $post_id; }
-	if(defined('DOING_AJAX') && DOING_AJAX && (isset($post_id))) { return $post_id; } //Prevents the metaboxes from being overwritten while quick editing.
-	if(preg_match('/\edit\.php/', $_SERVER['REQUEST_URI']) && (isset($post_id))) { return $post_id; } //Detects if the save action is coming from a quick edit/batch edit.
-	if (isset($_POST['profile_email'])) { update_post_meta($post->ID, "profile_email", $_POST["profile_email"]); }
-	if (isset($_POST['profile_title'])) { update_post_meta($post->ID, "profile_title", $_POST["profile_title"]); }
-	if (isset($_POST['profile_phone'])) {
-		$new = preg_replace('/[^a-z0-9]+/i', '', $_POST["profile_phone"]);
-		update_post_meta($post->ID, "profile_phone", $new);
-	}
+  global $post;
+  if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE && (isset($post_id))) { return $post_id; }
+  if(defined('DOING_AJAX') && DOING_AJAX && (isset($post_id))) { return $post_id; } //Prevents the metaboxes from being overwritten while quick editing.
+  if(preg_match('/\edit\.php/', $_SERVER['REQUEST_URI']) && (isset($post_id))) { return $post_id; } //Detects if the save action is coming from a quick edit/batch edit.
+  if (isset($_POST['profile_email'])) { update_post_meta($post->ID, "profile_email", $_POST["profile_email"]); }
+  if (isset($_POST['profile_title'])) { update_post_meta($post->ID, "profile_title", $_POST["profile_title"]); }
+  if (isset($_POST['profile_phone'])) {
+    $new = preg_replace('/[^a-z0-9]+/i', '', $_POST["profile_phone"]);
+    update_post_meta($post->ID, "profile_phone", $new);
+  }
 }
 
 /*
  * Create cystom functions to display data
  */
 function profile_title() {
-	global $post;
-	$custom = get_post_custom($post->ID);
-	if (isset($custom['profile_title'])) { echo $custom["profile_title"][0]; }
+  global $post;
+  $custom = get_post_custom($post->ID);
+  if (isset($custom['profile_title'])) { echo $custom["profile_title"][0]; }
 }
 function profile_email() {
-	global $post;
-	$custom = get_post_custom($post->ID);
-	if (isset($custom['profile_email'])) { echo $custom["profile_email"][0]; }
+  global $post;
+  $custom = get_post_custom($post->ID);
+  if (isset($custom['profile_email'])) { echo $custom["profile_email"][0]; }
 }
 function profile_phone($pattern = "$1.$2.$3") {
-	global $post;
-	$custom = get_post_custom($post->ID);
-	if (isset($custom['profile_phone'])) { echo preg_replace("/([0-9]{3})([0-9]{3})([0-9]{4})/", $pattern, $custom["profile_phone"][0]); }
+  global $post;
+  $custom = get_post_custom($post->ID);
+  if (isset($custom['profile_phone'])) { echo preg_replace("/([0-9]{3})([0-9]{3})([0-9]{4})/", $pattern, $custom["profile_phone"][0]); }
 }
 ?>
