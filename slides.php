de4f04b8199ea806e3cf5b8bf789ff618f9f0e9e
Matt Beall <mbeall@starverte.com>
2013-12-15 18:01:56 -0700

    Update description for Slides module

--- slides.php
+++ slides.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Create a slideshow made up of images and video
+ * Allows creation of media slideshows (a.k.a. carousels, sliders, etc.) for use in template files, posts, pages and more
  * Uses Bootstrap Carousel plugin
  *
  * @package Steel
