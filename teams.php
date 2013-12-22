a6155a286c86206980fe562797e7b5cdf07bde9d
Matt Beall <mbeall@starverte.com>
2013-12-21 19:23:31 -0700

    Remove is_module_active from teams.php (No longer necessary)

--- teams.php
+++ teams.php
@@ -10,7 +10,7 @@
 /*
  * Create custom post type
  */
-if (is_module_active('teams')) { add_action( 'init', 'steel_teams_init', 0 ); }
+add_action( 'init', 'steel_teams_init', 0 );
 function steel_teams_init() {
   $labels = array(
     'name'                => _x( 'Profiles', 'Post Type General Name', 'steel' ),
