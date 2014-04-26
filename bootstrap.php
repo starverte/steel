0f41860845464ce9efaa1996581b6cfd1d183979
Matt Beall <mbeall@starverte.com>
2014-04-25 18:47:56 -0600

    Remove comment leftover from WordPress Core

--- bootstrap.php
+++ bootstrap.php
@@ -5,18 +5,11 @@
  * @package Steel/Bootstrap
  * @uses Walker_Nav_Menu
  */
+
 class Steel_Bootstrap_List_Group extends Walker_Nav_Menu {
 
   /**
    * Starts the list before the elements are added.
-   *
-   * @see Walker::start_lvl()
-   *
-   * @since 3.0.0
-   *
-   * @param string $output Passed by reference. Used to append additional content.
-   * @param int    $depth  Depth of menu item. Used for padding.
-   * @param array  $args   An array of arguments. @see wp_nav_menu()
    */
   function start_lvl( &$output, $depth = 0, $args = array() ) {
     $indent = str_repeat("\t", $depth);
@@ -25,14 +18,6 @@ class Steel_Bootstrap_List_Group extends Walker_Nav_Menu {
 
   /**
    * Ends the list of after the elements are added.
-   *
-   * @see Walker::end_lvl()
-   *
-   * @since 3.0.0
-   *
-   * @param string $output Passed by reference. Used to append additional content.
-   * @param int    $depth  Depth of menu item. Used for padding.
-   * @param array  $args   An array of arguments. @see wp_nav_menu()
    */
   function end_lvl( &$output, $depth = 0, $args = array() ) {
     $indent = str_repeat("\t", $depth);
@@ -41,16 +26,6 @@ class Steel_Bootstrap_List_Group extends Walker_Nav_Menu {
 
   /**
    * Start the element output.
-   *
-   * @see Walker::start_el()
-   *
-   * @since 3.0.0
-   *
-   * @param string $output Passed by reference. Used to append additional content.
-   * @param object $item   Menu item data object.
-   * @param int    $depth  Depth of menu item. Used for padding.
-   * @param array  $args   An array of arguments. @see wp_nav_menu()
-   * @param int    $id     Current item ID.
    */
   function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
     $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
@@ -62,24 +37,12 @@ class Steel_Bootstrap_List_Group extends Walker_Nav_Menu {
 
     /**
      * Filter the CSS class(es) applied to a menu item's <li>.
-     *
-     * @since 3.0.0
-     *
-     * @param array  $classes The CSS classes that are applied to the menu item's <li>.
-     * @param object $item    The current menu item.
-     * @param array  $args    An array of arguments. @see wp_nav_menu()
      */
     $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
     $class_names = $class_names ? ' class="list-group-item ' . esc_attr( $class_names ) . '"' : '';
 
     /**
      * Filter the ID applied to a menu item's <li>.
-     *
-     * @since 3.0.1
-     *
-     * @param string The ID that is applied to the menu item's <li>.
-     * @param object $item The current menu item.
-     * @param array $args An array of arguments. @see wp_nav_menu()
      */
     $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
     $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
@@ -92,19 +55,6 @@ class Steel_Bootstrap_List_Group extends Walker_Nav_Menu {
 
     /**
      * Filter the HTML attributes applied to a menu item's <a>.
-     *
-     * @since 3.6.0
-     *
-     * @param array $atts {
-     *     The HTML attributes applied to the menu item's <a>, empty strings are ignored.
-     *
-     *     @type string $title  The title attribute.
-     *     @type string $target The target attribute.
-     *     @type string $rel    The rel attribute.
-     *     @type string $href   The href attribute.
-     * }
-     * @param object $item The current menu item.
-     * @param array  $args An array of arguments. @see wp_nav_menu()
      */
     $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
 
@@ -125,32 +75,12 @@ class Steel_Bootstrap_List_Group extends Walker_Nav_Menu {
 
     /**
      * Filter a menu item's starting output.
-     *
-     * The menu item's starting output only includes $args->before, the opening <a>,
-     * the menu item's title, the closing </a>, and $args->after. Currently, there is
-     * no filter for modifying the opening and closing <li> for a menu item.
-     *
-     * @since 3.0.0
-     *
-     * @param string $item_output The menu item's starting HTML output.
-     * @param object $item        Menu item data object.
-     * @param int    $depth       Depth of menu item. Used for padding.
-     * @param array  $args        An array of arguments. @see wp_nav_menu()
      */
     $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
   }
 
   /**
    * Ends the element output, if needed.
-   *
-   * @see Walker::end_el()
-   *
-   * @since 3.0.0
-   *
-   * @param string $output Passed by reference. Used to append additional content.
-   * @param object $item   Page data object. Not used.
-   * @param int    $depth  Depth of page. Not Used.
-   * @param array  $args   An array of arguments. @see wp_nav_menu()
    */
   function end_el( &$output, $item, $depth = 0, $args = array() ) {
     $output .= "\n";
