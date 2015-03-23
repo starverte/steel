e9e66a803533fd36a2a0818c1193752da21518df
Matt Beall <mbeall@starverte.com>
2015-03-23 13:35:42 -0600

    Only load GA script if user is not logged in

--- steel.php
+++ steel.php
@@ -308,16 +308,21 @@ function steel_ga_script() {
 
   $ga_id = $options['ga_id'];
 
-  if (!empty($ga_id)) { ?>
-    <script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', '<?php echo $ga_id; ?>']);
-      _gaq.push(['_trackPageview']);
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-    </script><?php
+  if (!empty($ga_id)) {
+    if (is_logged_in()) { ?>
+      <!-- Google Analtyics code disabled because user is logged in. --><?php
+    }
+    else { ?>
+      <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', '<?php echo $ga_id; ?>']);
+        _gaq.push(['_trackPageview']);
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+      </script><?php
+    }
   }
 }
