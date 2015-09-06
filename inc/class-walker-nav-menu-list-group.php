a87ee56b5cd8e5a8ddafee258e001748283d7223
Matt Beall <mbeall@starverte.com>
2015-09-05 23:05:56 -0600

    Rename and move class Steel_Walker_Nav_Menu_List_Group to its own file

---
+++ inc/class-walker-nav-menu-list-group.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ *
+ */
+class Steel_Walker_Nav_Menu_List_Group extends Walker_Nav_Menu {
+
+  /**
+   * Starts the list before the elements are added.
+   */
+  function start_lvl( &$output, $depth = 0, $args = array() ) {
+    $indent = str_repeat("\t", $depth);
+    $output .= "\n$indent<div class=\"sub-menu\">\n";
+  }
+
+  /**
+   * Ends the list of after the elements are added.
+   */
+  function end_lvl( &$output, $depth = 0, $args = array() ) {
+    $indent = str_repeat("\t", $depth);
+    $output .= "$indent</div>\n";
+  }
+
+  /**
+   * Start the element output.
+   */
+  function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+    $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+    $class_names = $value = '';
+
+    $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+    $classes[] = 'menu-item-' . $item->ID;
+
+    /**
+     * Filter the CSS class(es) applied to a menu item's <li>.
+     */
+    $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+    $class_names = $class_names ? ' class="list-group-item ' . esc_attr( $class_names ) . '"' : '';
+
+    /**
+     * Filter the ID applied to a menu item's <li>.
+     */
+    $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+    $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+    $atts = array();
+    $atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+    $atts['target'] = ! empty( $item->target )     ? $item->target     : '';
+    $atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
+    $atts['href']   = ! empty( $item->url )        ? $item->url        : '';
+
+    /**
+     * Filter the HTML attributes applied to a menu item's <a>.
+     */
+    $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
+
+    $attributes = '';
+    foreach ( $atts as $attr => $value ) {
+      if ( ! empty( $value ) ) {
+        $value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+        $attributes .= ' ' . $attr . '="' . $value . '"';
+      }
+    }
+
+    $item_output = $args->before;
+    $item_output .= '<a ' . $id . $class_names . $attributes .'>';
+    /** This filter is documented in wp-includes/post-template.php */
+    $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+    $item_output .= '</a>';
+    $item_output .= $args->after;
+
+    /**
+     * Filter a menu item's starting output.
+     */
+    $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+  }
+
+  /**
+   * Ends the element output, if needed.
+   */
+  function end_el( &$output, $item, $depth = 0, $args = array() ) {
+    $output .= "\n";
+  }
+
+}
