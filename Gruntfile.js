aa740268c30bc515773d7adbdd671790aecde118
Matt Beall <mbeall@starverte.com>
2015-10-04 22:11:07 -0600

    Add grunt-phpdoc

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,9 +26,34 @@ module.exports = function(grunt) {
       phpcs_tests: {
         command: 'tests/php-codesniffer/scripts/phpcs -p -s -v -n . --standard=./.phpcs.rules.xml --extensions=php --ignore=tests/*,node_modules/* >> tests/results'
       }
+    },
+    phpdoc: {
+      options: {
+        // Task-specific options go here.
+      },
+      docs: [
+        'bootstrap.php',
+        'broadcast.php',
+        'deprecated.php',
+        'inc/class-walker-nav-menu-list-group.php',
+        'inc/class-widget-button.php',
+        'inc/class-widget-link.php',
+        'inc/class-widget-list-group.php',
+        'inc/class-widget-random-quote.php',
+        'options.php',
+        'quotes.php',
+        'shortcodes.php',
+        'slides.php',
+        'social_media.php',
+        'steel.php',
+        'teams.php',
+        'templates.php',
+        'widgets.php'
+      ]
     }
   });
 
+  grunt.loadNpmTasks( 'grunt-phpdoc' );
   grunt.loadNpmTasks( 'grunt-shell' );
   grunt.registerTask( 'init', ['shell:empty_tests', 'shell:syntax_clone', 'shell:phpcs_clone', 'shell:wpcs_clone', 'shell:phpcs_config'] );
   grunt.registerTask( 'test', ['shell:reset_tests', 'shell:syntax_tests', 'shell:phpcs_tests'] );
