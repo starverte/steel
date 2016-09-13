18903420a403254f1214e06540b69610c5fb2e78
Matt Beall <mbeall@starverte.com>
2016-09-13 11:56:00 -0600

    Grunt: Tidy config and update npm, composer

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,25 +36,46 @@ module.exports = function(grunt) {
       },
     },
     shell: {
-      phpcs_config: {
-        command: 'vendor/bin/phpcs --config-set installed_paths ../../wp-coding-standards/wpcs'
-      },
-      syntax_tests: {
-        command: "find . -name '*.php' -not -path './node_modules/*' -not -path './vendor/*' -exec php -lf '{}' \\;"
-      },
-      phpcs_tests: {
-        command: 'vendor/bin/phpcs -p -s -v -n . --standard=./.phpcs.rules.xml --extensions=php --ignore=node_modules/*,vendor/*'
+      composer_update: {
+        command: 'composer update'
       },
       msx_cards: {
         command: 'sed -i "" "s/MSX_TEXT_DOMAIN/steel/" cards/*'
+      },
+      npm_update: {
+        command: 'npm update --save --save-dev'
+      },
+      phpcs_config: {
+        command: 'vendor/bin/phpcs --config-set installed_paths ../../wp-coding-standards/wpcs'
+      },
+      phpcs_tests: {
+        command: 'vendor/bin/phpcs -p -s -v -n . --standard=./.phpcs.rules.xml --extensions=php --ignore=deprecated/*,node_modules/*,vendor/*'
+      },
+      syntax_tests: {
+        command: "find . -name '*.php' -not -path './node_modules/*' -not -path './vendor/*' -exec php -lf '{}' \\;"
       }
     }
   });
 
   grunt.loadNpmTasks( 'grunt-contrib-copy' );
   grunt.loadNpmTasks( 'grunt-shell' );
-  grunt.registerTask( 'init', ['shell:phpcs_config'] );
-  grunt.registerTask( 'build', ['copy:bootstrap', 'copy:matchstix', 'shell:msx_cards'] );
-  grunt.registerTask( 'test', ['shell:syntax_tests', 'shell:phpcs_tests'] );
-}
 
+  grunt.registerTask( 'init', [
+    'shell:npm_update',
+    'shell:composer_update',
+    'shell:phpcs_config'
+  ] );
+
+  grunt.registerTask( 'build', [
+    'init',
+    'copy:bootstrap',
+    'copy:matchstix',
+    'shell:msx_cards'
+  ] );
+
+  grunt.registerTask( 'test', [
+    'build',
+    'shell:syntax_tests',
+    'shell:phpcs_tests'
+  ] );
+}
