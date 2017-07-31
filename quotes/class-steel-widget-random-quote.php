e4166f2d8c5aeea66d1914ed2b0585f9a26e2144
Matt Beall <mbeall@starverte.com>
2017-07-31 14:10:40 -0600

    WPCS: Pass WordPress.Arrays.CommaAfterArrayItem

--- quotes/class-steel-widget-random-quote.php
+++ quotes/class-steel-widget-random-quote.php
@@ -17,16 +17,16 @@ class Steel_Widget_Random_Quote extends WP_Widget {
    * PHP5 constructor.
    */
   function __construct() {
-		$widget_ops = array(
-		'classname' => 'steel-widget-random-quote',
-		'description' => __( 'Displays a random quote', 'steel' ),
-		);
-		$control_ops = array(
-		'width' => 300,
-		'height' => 350,
-		'id_base' => 'steel-widget-random-quote',
-		);
-		parent::__construct( 'steel-widget-random-quote', __( 'Steel: Random Quote', 'steel' ), $widget_ops, $control_ops );
+    $widget_ops = array(
+      'classname' => 'steel-widget-random-quote',
+      'description' => __( 'Displays a random quote', 'steel' ),
+    );
+    $control_ops = array(
+      'width' => 300,
+      'height' => 350,
+      'id_base' => 'steel-widget-random-quote',
+    );
+    parent::__construct( 'steel-widget-random-quote', __( 'Steel: Random Quote', 'steel' ), $widget_ops, $control_ops );
   }
 
   /**
@@ -40,41 +40,41 @@ class Steel_Widget_Random_Quote extends WP_Widget {
     $cat = ! empty( $instance['cat'] ) ? get_category( $instance['cat'] ) : false;
 
     if ( ! $cat ) {
-        return;
-        }
+      return;
+    }
 
     $instance['title'] = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
 
     echo $args['before_widget'];
 
     if ( ! empty( $instance['title'] ) ) {
-        echo $args['before_title'] . $instance['title'] . $args['after_title'];
-        }
+      echo $args['before_title'] . $instance['title'] . $args['after_title'];
+    }
 
     $quotes = new WP_Query(
-    array(
-    'post_type' => 'post',
-    'orderby' => 'rand',
-    'posts_per_page' => 1,
-    'tax_query' => array(
-      'relation' => 'AND',
       array(
-        'taxonomy' => 'category',
-        'field' => 'slug',
-        'terms' => $cat->slug,
-      ),
-      array(
-        'taxonomy' => 'post_format',
-        'field' => 'slug',
-        'terms' => array( 'post-format-quote' ),
+        'post_type' => 'post',
+        'orderby' => 'rand',
+        'posts_per_page' => 1,
+        'tax_query' => array(
+          'relation' => 'AND',
+          array(
+            'taxonomy' => 'category',
+            'field' => 'slug',
+            'terms' => $cat->slug,
+          ),
+          array(
+            'taxonomy' => 'post_format',
+            'field' => 'slug',
+            'terms' => array( 'post-format-quote' ),
+          ),
+        ),
       )
-    ),
-    )
     );
 
     while ( $quotes->have_posts() ) :
       $quotes->the_post(); ?>
-        <blockquote><?php the_content(); ?></blockquote>
+      <blockquote><?php the_content(); ?></blockquote>
       <?php
     endwhile;
 
@@ -94,9 +94,9 @@ class Steel_Widget_Random_Quote extends WP_Widget {
    * @return array Settings to save or bool false to cancel saving.
    */
   function update( $new_instance, $old_instance ) {
-		$instance['title'] = strip_tags( stripslashes( $new_instance['title'] ) );
-		$instance['list'] = (int) $new_instance['list'];
-		return $instance;
+    $instance['title'] = strip_tags( stripslashes( $new_instance['title'] ) );
+    $instance['list'] = (int) $new_instance['list'];
+    return $instance;
   }
 
   /**
@@ -105,29 +105,29 @@ class Steel_Widget_Random_Quote extends WP_Widget {
    * @param array $instance Current settings.
    */
   function form( $instance ) {
-		$title = ! empty( $instance['title'] ) ? $instance['title'] : '';
-		$list  = ! empty( $instance['list'] ) ? $instance['list'] : '';
+    $title = ! empty( $instance['title'] ) ? $instance['title'] : '';
+    $list  = ! empty( $instance['list'] ) ? $instance['list'] : '';
 
-		$cats = get_categories();
-		?>
-		<p>
-		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-		<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $title; ?>" />
-		</p>
-		<p>
-		<label for="<?php echo $this->get_field_id( 'list' ); ?>"><?php _e( 'Category:' ); ?></label>
-		<select id="<?php echo $this->get_field_id( 'list' ); ?>" name="<?php echo $this->get_field_name( 'list' ); ?>">
+    $cats = get_categories();
+    ?>
+    <p>
+      <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+      <input type="text" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $title; ?>" />
+    </p>
+    <p>
+      <label for="<?php echo $this->get_field_id( 'list' ); ?>"><?php _e( 'Category:' ); ?></label>
+      <select id="<?php echo $this->get_field_id( 'list' ); ?>" name="<?php echo $this->get_field_name( 'list' ); ?>">
         <option value="">All Categories</option>
         <?php
-          foreach ( $cats as $cat ) {
-            $option  = '<option value="' . $cat->term_id . '" ' . selected( $list, $cat->term_id ) . '>';
-            $option .= $cat->cat_name;
-            $option .= '</option>';
-            echo $option;
-			}
+        foreach ( $cats as $cat ) {
+          $option  = '<option value="' . $cat->term_id . '" ' . selected( $list, $cat->term_id ) . '>';
+          $option .= $cat->cat_name;
+          $option .= '</option>';
+          echo $option;
+        }
         ?>
-		</select>
-		</p>
-		<?php
+      </select>
+    </p>
+    <?php
   }
 }
