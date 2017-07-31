ce2be52dbe7d842dd886687ecb58b6b33b27d126
Matt Beall <mbeall@starverte.com>
2017-07-31 13:21:44 -0600

    Realign Steel_Widget_Button

--- widgets/class-steel-widget-button.php
+++ widgets/class-steel-widget-button.php
@@ -17,16 +17,16 @@ class Steel_Widget_Button extends WP_Widget {
    * PHP5 constructor.
    */
   function __construct() {
-		$widget_ops = array(
-		'classname' => 'steel-widget-button',
-		'description' => __( 'A widget that displays a linked button', 'steel' ),
-		);
-		$control_ops = array(
-		'width' => 300,
-		'height' => 350,
-		'id_base' => 'steel-widget-button',
-		);
-		parent::__construct( 'steel-widget-button', __( 'Steel: Button', 'steel' ), $widget_ops, $control_ops );
+    $widget_ops = array(
+      'classname' => 'steel-widget-button',
+      'description' => __( 'A widget that displays a linked button', 'steel' ),
+    );
+    $control_ops = array(
+      'width' => 300,
+      'height' => 350,
+      'id_base' => 'steel-widget-button',
+    );
+    parent::__construct( 'steel-widget-button', __( 'Steel: Button', 'steel' ), $widget_ops, $control_ops );
   }
 
   /**
@@ -42,31 +42,31 @@ class Steel_Widget_Button extends WP_Widget {
     $href = $instance['href'];
     $style = strtolower( $instance['style'] );
     switch ( $style ) {
-        case 'default':
-            $style = 'btn btn-block btn-default';
+      case 'default':
+        $style = 'btn btn-block btn-default';
         break;
-        case 'primary':
-            $style = 'btn btn-block btn-primary';
+      case 'primary':
+        $style = 'btn btn-block btn-primary';
         break;
-        case 'info':
-            $style = 'btn btn-block btn-info';
+      case 'info':
+        $style = 'btn btn-block btn-info';
         break;
-        case 'success':
-            $style = 'btn btn-block btn-success';
+      case 'success':
+        $style = 'btn btn-block btn-success';
         break;
-        case 'warning':
-            $style = 'btn btn-block btn-warning';
+      case 'warning':
+        $style = 'btn btn-block btn-warning';
         break;
-        case 'danger':
-            $style = 'btn btn-block btn-danger';
+      case 'danger':
+        $style = 'btn btn-block btn-danger';
         break;
-        case 'inverse':
-            $style = 'btn btn-block btn-inverse';
+      case 'inverse':
+        $style = 'btn btn-block btn-inverse';
         break;
-        case 'link':
-            $style = 'btn btn-block btn-link';
+      case 'link':
+        $style = 'btn btn-block btn-link';
         break;
-        }
+    }
     $show_info = isset( $instance['show_info'] ) ? $instance['show_info'] : false;
     if ( $title ) {
       echo '<p><a class="' . $style . '" href=' . $href . ' type="button">' . $title . '</a></p>';
@@ -86,11 +86,11 @@ class Steel_Widget_Button extends WP_Widget {
    * @return array Settings to save or bool false to cancel saving.
    */
   function update( $new_instance, $old_instance ) {
-		$instance = $old_instance;
-		$instance['title'] = strip_tags( $new_instance['title'] );
-		$instance['href']  = strip_tags( $new_instance['href'] );
-		$instance['style'] = strip_tags( $new_instance['style'] );
-		return $instance;
+    $instance = $old_instance;
+    $instance['title'] = strip_tags( $new_instance['title'] );
+    $instance['href']  = strip_tags( $new_instance['href'] );
+    $instance['style'] = strip_tags( $new_instance['style'] );
+    return $instance;
   }
 
   /**
@@ -103,19 +103,19 @@ class Steel_Widget_Button extends WP_Widget {
     $href  = ! empty( $instance['href'] )  ? $instance['href']  : 'http://';
     $style = ! empty( $instance['style'] ) ? $instance['style'] : ''; ?>
 
-      <p>
+    <p>
       <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'steel' ); ?></label>
       <input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $title; ?>" style="width:100%;" />
-      </p>
-      <p>
+    </p>
+    <p>
       <label for="<?php echo $this->get_field_id( 'href' ); ?>"><?php _e( 'Link:', 'steel' ); ?></label>
       <input id="<?php echo $this->get_field_id( 'href' ); ?>" name="<?php echo $this->get_field_name( 'href' ); ?>" value="<?php echo $href; ?>" style="width:100%;" />
-      </p>
-      <p>
+    </p>
+    <p>
       <label for="<?php echo $this->get_field_id( 'style' ); ?>"><?php _e( 'Button Style:', 'steel' ); ?></label>
       <input id="<?php echo $this->get_field_id( 'style' ); ?>" name="<?php echo $this->get_field_name( 'style' ); ?>" value="<?php echo $style; ?>" style="width:100%;" />
-      </p>
-      <div class="btn-group" data-toggle="buttons-radio">
+    </p>
+    <div class="btn-group" data-toggle="buttons-radio">
       <button type="button" class="btn">Default</button>
       <button type="button" class="btn btn-primary">Primary</button>
       <button type="button" class="btn btn-info">Info</button>
@@ -124,8 +124,7 @@ class Steel_Widget_Button extends WP_Widget {
       <button type="button" class="btn btn-danger">Danger</button>
       <button type="button" class="btn btn-inverse">Inverse</button>
       <button type="button" class="btn btn-link">Link</button>
-      </div>
-      <?php
+    </div>
+    <?php
   }
 }
-
